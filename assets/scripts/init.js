c41cbc1d87e66659e44df03afecd552794114b62
Erik Salazar <3salaz.dev@gmail.com>
2020-04-30 13:40:03 -0700

    added logo, photography and videos into mix

--- assets/scripts/init.js
+++ assets/scripts/init.js
@@ -2,5 +2,8 @@
     $(function(){
         $('.sidenav').sidenav();
         $('.parallax').parallax();
+        $('.modal').modal();
+        $('.slider').slider()
+        $('.fixed-action-btn').floatingActionButton();
     }); // end of document ready
 })(jQuery); // end of jQuery name space
